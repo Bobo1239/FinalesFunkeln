7f3b8ab6099297075951d9c435f87e32b7919c2b
Daniel Lenhardt <ckwdani@gmail.com>
2018-11-17 23:48:37 +0100

    Add Lambertian functionalities to material.rs

--- src/material.rs
+++ src/material.rs
@@ -1,25 +1,41 @@
 use ray::Ray;
 use vec3::Vec3;
+use rand::Rng;
 
-struct HitRecord;
+struct HitRecord{
+	p: Vec3,
+	normal: Vec3,
+}
 
 trait Scatter {
-	fn scatter(&self, ray: &Ray, attenuation: &Vec3, hit_record: &HitRecord) -> (Ray, f32);
+	fn scatter(&self, ray: &Ray, attenuation: &Vec3, hit_record: &HitRecord) -> (Ray, Vec3);
 }
 
 struct Lambertian {
-	albedo: f32,
+	albedo: Vec3,
 }
 
 impl Scatter for Lambertian {
-	fn scatter(&self, ray: &Ray, attenuation: &Vec3, hit_record: &HitRecord) -> (Ray, f32) {
-
+	fn scatter(&self, _ray: &Ray, _attenuation: &Vec3, hit_record: &HitRecord) -> (Ray, Vec3) {
+		let target: Vec3 = hit_record.p + hit_record.normal + random_in_sphere();
+		let scattered: Ray = Ray::new(hit_record.p, target - hit_record.p);
+		return (scattered, self.albedo);
 	}
 }
 
 
 fn random_in_sphere() -> Vec3{
 	let mut p: Vec3;
+	let mut rng = rand::thread_rng();
+
+	loop {
+		p = Vec3::new(rng.gen(), rng.gen(), rng.gen())*2.0 - Vec3::new(1., 1., 1.);
+		if p.length_squared() >= 1.0{
+			return p;
+		}
+	}
+
+
 	
 }
 
