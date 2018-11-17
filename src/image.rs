275e851838a4658ab4d88b951bec11f944052804
Daniel Lenhardt <ckwdani@gmail.com>
2018-11-17 16:20:03 +0100

    Implement save_to_ppm

--- src/image.rs
+++ src/image.rs
@@ -1,6 +1,29 @@
+use std::fs::File;
+use std::io::{self, Write};
+use std::path::Path;
+
 use vec3::Vec3;
 
 #[derive(Debug)]
 pub struct Image {
     image: Vec<Vec<Vec3>>,
+}
+
+impl Image {
+    pub fn save_to_ppm(&self, path: &Path) -> Result<(), io::Error> {
+        let mut file = File::create(&path)?;
+
+        let header = format!("P3 {} {} 255\n", self.image.len(), self.image[0].len());
+        file.write(header.as_bytes())?;
+
+        for row in &self.image {
+            for pixel in row {
+                let pixel_val = format!("{} {} {}   ", pixel.r(), pixel.g(), pixel.b());
+                file.write(pixel_val.as_bytes())?;
+            }
+            file.write("\n".as_bytes())?;
+        }
+
+        Ok(())
+    }
 }
