8f79d172007f98fb71484421e55ca0f2a3a5a02a
Boris-Chengbiao Zhou <bobo1239@web.de>
2018-11-17 05:02:48 +0100

    Add dummies for all Vec3 functions

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,3 +1,6 @@
+use core::ops::*;
+use std::fmt::{self, Display, Formatter};
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Vec3([f32; 3]);
 
@@ -28,5 +31,142 @@ impl Vec3 {
 
     pub fn b(&self) -> f32 {
         self.0[2]
+    }
+}
+
+impl Vec3 {
+    pub fn length(&self) -> f32 {
+        unimplemented!();
+    }
+
+    pub fn length_squared(&self) -> f32 {
+        unimplemented!();
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        unimplemented!();
+    }
+
+    pub fn make_unit_vector(&mut self) {
+        unimplemented!();
+    }
+
+    pub fn dot(&self, other: &Vec3) -> f32 {
+        unimplemented!();
+    }
+
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        unimplemented!();
+    }
+}
+
+impl Index<usize> for Vec3 {
+    type Output = f32;
+    fn index(&self, idx: usize) -> &f32 {
+        unimplemented!();
+    }
+}
+
+impl IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, idx: usize) -> &mut f32 {
+        unimplemented!();
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+    fn neg(self) -> Vec3 {
+        unimplemented!();
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+    fn add(self, other: Vec3) -> Vec3 {
+        unimplemented!();
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+    fn sub(self, other: Vec3) -> Vec3 {
+        unimplemented!();
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+    fn mul(self, other: Vec3) -> Vec3 {
+        unimplemented!();
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Vec3;
+    fn div(self, other: Vec3) -> Vec3 {
+        unimplemented!();
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, other: f32) -> Vec3 {
+        unimplemented!();
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Vec3;
+    fn div(self, other: f32) -> Vec3 {
+        unimplemented!();
+    }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+    fn mul(self, other: Vec3) -> Vec3 {
+        unimplemented!();
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Vec3) {
+        unimplemented!();
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Vec3) {
+        unimplemented!();
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, other: Vec3) {
+        unimplemented!();
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, other: Vec3) {
+        unimplemented!();
+    }
+}
+
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, other: f32) {
+        unimplemented!();
+    }
+}
+
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, other: f32) {
+        unimplemented!();
+    }
+}
+
+impl Display for Vec3 {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "({}, {}, {})", self.x(), self.y(), self.z())
     }
 }
